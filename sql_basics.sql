8f67efe74c7dce6be9ccde3ca14df3bb0cddefe8
Abhishek Potnis <63377959+abhi0903-stack@users.noreply.github.com>
2026-01-13 00:21:51 +0000

    Update sql_basics.sql

--- sql_basics.sql
+++ sql_basics.sql
@@ -1,1 +1,16 @@
+/* Hacker Rank Question: Average Population of Each Continent
+
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
+
+SELECT co.continent, FLOOR(AVG(cy.population)) 
+FROM country co
+INNER JOIN city cy
+ON co.code = cy.countrycode
+GROUP BY co.continent; 
+
+
+
+
 
