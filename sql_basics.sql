f37a1cc45cfd28f98290557bdd0f1a7bdb3da5b0
Abhishek Potnis <63377959+abhi0903-stack@users.noreply.github.com>
2026-01-13 00:27:19 +0000

    Update sql_basics.sql

--- sql_basics.sql
+++ sql_basics.sql
@@ -2,7 +2,11 @@
 
 Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
 
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns.*/
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+--Concepts: Inner Joins
+
 
 SELECT co.continent, FLOOR(AVG(cy.population)) 
 FROM country co
@@ -10,7 +14,28 @@ INNER JOIN city cy
 ON co.code = cy.countrycode
 GROUP BY co.continent; 
 
+/*
+Hacker Rank Question: Type Of Triangle
 
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+Equilateral: It's a triangle with  sides of equal length.
+Isosceles: It's a triangle with  sides of equal length.
+Scalene: It's a triangle with  sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+
+-- Concepts: Case Statement
+
+SELECT 
+CASE 
+    WHEN (A+B)<=C OR (A+C)<=B OR (B+C)<=A THEN 'Not A Triangle'
+    WHEN (A=B) AND (B=C) THEN 'Equilateral'
+    WHEN (A=B) OR (B=C) OR (A=C) THEN 'Isosceles'
+    ELSE 'Scalene'
+    END AS type_of_traingle
+FROM
+TRIANGLES; 
 
 
 
