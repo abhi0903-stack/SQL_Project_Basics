985ed67021ccac56a477ca61f7b1f32542d76aa4
Abhishek Potnis <63377959+abhi0903-stack@users.noreply.github.com>
2026-01-13 01:24:10 +0000

    Update hacker_rank_easy_probs.sql

--- hacker_rank_easy_probs.sql
+++ hacker_rank_easy_probs.sql
@@ -70,4 +70,19 @@ Query the Manhattan Distance between points  and  and round it to a scale of  de
 SELECT ROUND(ABS(MIN(LAT_N)-MAX(LAT_N)) + ABS(MIN(LONG_W)-MAX(LONG_W)),4)
 FROM STATION;
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+/*
+Hacker Rank Question: Top Earners
+We define an employee's total earnings to be their monthly salary * months worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. 
+Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
+Then print these values as 2 space-separated integers.
+*/
+
+-- Concepts: Aggregate Functions
+SELECT (salary*months) AS earnings,
+COUNT(*)
+FROM Employee
+GROUP BY earnings
+ORDER BY earnings DESC 
+LIMIT 1; 
