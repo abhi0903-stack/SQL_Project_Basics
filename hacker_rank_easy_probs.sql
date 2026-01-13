9cdfc271086606a9db496500effe456f16c5455f
Abhishek Potnis <63377959+abhi0903-stack@users.noreply.github.com>
2026-01-13 01:08:44 +0000

    Update hacker_rank_easy_probs.sql

--- hacker_rank_easy_probs.sql
+++ hacker_rank_easy_probs.sql
@@ -13,6 +13,7 @@ FROM country co
 INNER JOIN city cy
 ON co.code = cy.countrycode
 GROUP BY co.continent; 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
 Hacker Rank Question: Type Of Triangle
@@ -36,6 +37,7 @@ CASE
     END AS type_of_traingle
 FROM
 TRIANGLES; 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
 Hacker Rank Question: African Cities
@@ -52,5 +54,20 @@ INNER JOIN country co
 ON cy.countrycode = co.code
 WHERE co.continent = 'Africa';
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+*/
+
+-- Concepts: Aggregate Functions 
+
+SELECT ROUND(ABS(MIN(LAT_N)-MAX(LAT_N)) + ABS(MIN(LONG_W)-MAX(LONG_W)),4)
+FROM STATION;
 
 
